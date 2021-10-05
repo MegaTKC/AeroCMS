fd92ee81bd6964a5d6861929783554a97429eb72
MegaTKC <officialmegatkc@gmail.com>
2021-10-05 14:34:48 -0700

    Update Site Widget Well

--- includes/widget.php
+++ includes/widget.php
@@ -1,4 +1,4 @@
 <div class="well">
     <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    <p>A blog website!</p>
 </div>

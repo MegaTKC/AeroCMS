0731b5db53f4b14c7f582bd08296f8bf732a3afd
MegaTKC <officialmegatkc@gmail.com>
2021-10-10 10:38:14 -0700

    Add Site Settings Page and Nav button

---
+++ admin/general-settings.php
@@ -0,0 +1,33 @@
+<?php include("includes/header.php"); ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("includes/navigation.php"); ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to the Admin Panel,
+                            <small><?php echo $_SESSION['username']; ?>!</small>
+                        </h1>
+                        <p>Coming Soon</p>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include("includes/footer.php"); ?>
